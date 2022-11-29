f9cec7c2878239fd690ece09c860086f271af538
oztur <oztur@YIIT>
2022-11-29 14:02:10 +0100

    Proje dosyası ekle.

---
+++ Ders123/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Welcome For Converting");
+Console.WriteLine("For Skipping Press Any Button");
+Console.ReadLine();
+Console.Clear();
+Console.WriteLine("Enter Your Word");
+string word;
+word = Console.ReadLine();   
+Console.Clear();
+string first = word.Substring(1); 
+string second = word.Substring(0,1);  
+Console.WriteLine(first + second + "ay");
+
+
